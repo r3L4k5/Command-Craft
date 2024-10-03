4d32cbcea232a89d9ea55431c7b88c51ac37bb6f
rELakS <aminjaber51@gmail.com>
2024-10-03 22:30:32 +0200

    Range detection area for npcs partially fixed

--- source/characters/npc/dog.py
+++ source/characters/npc/dog.py
@@ -11,24 +11,3 @@ class Dog(WorldObject, NPC):
         super().__init__("dog", " m", y, x, Category.ENEMY)
         NPC.__init__(self, 100, 10, 1, 3)
 
-    def interact(self, object: WorldObject):
-
-        if object.category == Category.PLAYER:
-            self.sprite = colored(self.sprite, "red")
-            object.health = 50
-    
-    def detection(self, world):
-
-        up_vision: int = clamp(self.y - self.vision, len(world), 0)
-        down_vision: int = clamp(self.y + self.vision, len(world), 0)
-
-        left_vision: int = clamp(self.x - self.vision, len(world[self.y]), 0)
-        right_vision: int = clamp(self.x - self.vision, len(world[self.y]), 0)
-        
-        for y in range(up_vision, down_vision):
-            for x in range(left_vision, right_vision):
-
-                self.interact(world[x][y])
-
-    def update_npc(self, world):
-        self.detection(world)
--- source/characters/npc/npc_class.py
+++ source/characters/npc/npc_class.py
@@ -1,6 +1,8 @@
 
 from characters.character_class import Character
+from systems.object_class import Category, WorldObject
 from utility import clamp
+from termcolor import colored
 
 class NPC(Character):
 
@@ -9,20 +11,28 @@ class NPC(Character):
         super().__init__(health, strength, speed)
 
         self.vision = vision
-    
+
+    def interact(self, object: WorldObject):
+        
+        if object.sprite == " i":
+            self.sprite = colored(self.sprite, "red")
+        
     def detection(self, world):
 
         up_vision: int = clamp(self.y - self.vision, len(world), 0)
         down_vision: int = clamp(self.y + self.vision, len(world), 0)
 
         left_vision: int = clamp(self.x - self.vision, len(world[self.y]), 0)
-        right_vision: int = clamp(self.x - self.vision, len(world[self.y]), 0)
+        right_vision: int = clamp(self.x + self.vision, len(world[self.y]), 0)
+
+        #Add 1 because the stop number is not included
+        #otherwise y and x will never reach max range
+        for y in range(up_vision, down_vision + 1):
+            for x in range(left_vision, right_vision + 1):
+                 
+                self.interact(world[y][x])
         
-        for y in range(up_vision, down_vision):
-            for x in range(left_vision, right_vision):
-
-                world[y][x].health = 50
-
+    
     def update_npc(self, world):
         self.detection(world)
 
